1b6def4486d0c9ac55781a9746a803ce33121ce3
Vitória Xavier <vitoriapxaviera@gmail.com>
2025-11-17 18:30:52 -0300

    feat: flask config

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'troca-essa-chave-por-uma-secreta'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'locacao.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
